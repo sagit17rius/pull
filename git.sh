a234c7c1cd3d14d5c6e86ba1faa6b5176cb33d06
sagit17rius <155385537+sagit17rius@users.noreply.github.com>
2024-09-04 00:35:39 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Directory name (passed as an argument or hardcoded)
-DIR_NAME=${1:-"my_directory"}
+DIR_NAME="my_directory"
 
-# Check if the directory exists
 if [ -d "$DIR_NAME" ]; then
   echo "Directory '$DIR_NAME' already exists."
 else
-  # Create the directory
+
   mkdir "$DIR_NAME"
   
   if [ $? -eq 0 ]; then
