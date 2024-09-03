3268550d1df56a97e7fc23fceb9784a23e5e6064
sagit17rius <155385537+sagit17rius@users.noreply.github.com>
2024-09-04 00:37:12 +0530

    git.sh

--- git.sh
+++ git.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
 
-DIR_NAME="my_directory"
-
-if [ -d "$DIR_NAME" ]; then
-  echo "Directory '$DIR_NAME' already exists."
-else
-
-  mkdir "$DIR_NAME"
+  mkdir "DIR_NAME"
   
-  if [ $? -eq 0 ]; then
-    echo "Directory '$DIR_NAME' created successfully."
-  else
-    echo "Failed to create directory '$DIR_NAME'."
-  fi
-fi
